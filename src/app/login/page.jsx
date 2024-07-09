641a8f545dd505086004ffc30f73b545e3c4a3c4
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-07-09 14:23:29 -0600

    [add] Instalación de Next.js, creación base de páginas de login y registro

---
+++ src/app/login/page.jsx
@@ -0,0 +1,8 @@
+
+export default function Login() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Login</h1>
+    </div>
+  );
+}
---
+++ src/app/register/page.jsx
@@ -0,0 +1,7 @@
+export default function Register() {
+    return (
+      <div className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Register</h1>
+      </div>
+    );
+  }
