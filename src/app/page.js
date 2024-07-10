b5d653cb5b84ef79ec728a37f9c7887159513f6d
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-07-10 15:38:57 -0600

    [change] Renderiza el sidebar y el table container. Ajuste de estilos

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+import { ProductTable } from "@/components/ProductTable";
+import { Sidebar } from "@/components/Sidebar";
+import { TableContainer } from "@/components/TableContainer";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Pedidos</h1>
+    <main className="flex min-h-screen">
+      <Sidebar />
+      <TableContainer />
     </main>
   );
 }
