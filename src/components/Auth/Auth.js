8ecaed6aeaa9632d228ebb4190cbc7813a9c51e3
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-07-09 19:43:03 -0600

    [add] Implementación de control de estado isAuthChecked para evitar redireccionamientos innecesarios en el componente Auth
    - Agregado estado isAuthChecked para controlar la verificación de autenticación.
    - La lógica de redireccionamiento ahora se ejecuta solo una vez después de la primera verificación de autenticación.
    - Previene ciclos de redireccionamiento y mejora la eficiencia del componente Auth.

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -2,19 +2,24 @@
 import useLocalStorage from "@/hooks/useLocalStorage";
 import { authKey } from "@/lib/constants";
 import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Auth = () => {
-    const [storedValue] = useLocalStorage(authKey);
+    const [storedValue] = useLocalStorage(authKey, null);
+    const [isAuthChecked, setAuthChecked] = useState(false)
     const router = useRouter();
     const pathname = usePathname();
     useEffect(() => {
-        if(storedValue) {
-            router.push('/')
-        } else if(pathname !== '/register'){
-            router.push('/login')
+        if (!isAuthChecked) {
+            if (storedValue) {
+                router.push('/');
+            } else if (pathname !== '/register') {
+                router.push('/login');
+            }
+            setAuthChecked(true);
         }
-    }, [storedValue, router, pathname])
+
+    }, [storedValue, router, pathname, isAuthChecked])
    
 
     return null
