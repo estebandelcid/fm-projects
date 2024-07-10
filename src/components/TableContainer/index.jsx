45e8d133dad05a47f514827c1cf1066c34866913
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-07-10 15:37:37 -0600

    [add] Componente TableContainer que envuelve al componente de la tabla. Maquetación de status de los productos

---
+++ src/components/TableContainer/index.jsx
@@ -0,0 +1,48 @@
+import { ProductTable } from "../ProductTable";
+import {
+  ClockIcon,
+  DocumentCheckIcon,
+  DocumentTextIcon,
+  XCircleIcon,
+} from "@heroicons/react/24/outline";
+
+export const TableContainer = () => {
+  return (
+    <div className="ml-52 w-full h-full p-4">
+      <h1 className="text-white text-3xl font-bold underline underline-offset-8">
+        PEDIDOS
+      </h1>
+      <section className=" w-8/12 flex justify-between py-6">
+        <div className="w-40 flex justify-between items-center rounded-sm bg-[#f7f7f7] px-6 py-4">
+          <DocumentTextIcon className="size-11" />
+          <p className="flex flex-col justify-center items-center">
+            <span className="font-semibold text-lg">50</span>
+            <span className="text-xs">pedidos</span>
+          </p>
+        </div>
+        <div className="w-40 bg-[#f7f7f7] flex justify-between items-center px-6 py-4">
+          <ClockIcon className="size-11 text-[#027bcb]" />
+          <p className="flex flex-col justify-center items-center">
+            <span className="font-semibold text-lg">50</span>
+            <span className="text-xs">en proceso</span>
+          </p>
+        </div>
+        <div className="w-40 bg-[#f7f7f7] flex justify-between items-center px-6 py-4">
+          <XCircleIcon className="size-11 text-[#f88080]" />
+          <p className="flex flex-col justify-center items-center">
+            <span className="font-semibold text-lg">50</span>
+            <span className="text-xs">rechazado</span>
+          </p>
+        </div>
+        <div className="w-40 bg-[#f7f7f7] flex justify-between items-center px-6 py-4">
+          <DocumentCheckIcon className="size-11 text-[#96d871]" />
+          <p className="flex flex-col justify-center items-center">
+            <span className="font-semibold text-lg">50</span>
+            <span className="text-xs">listo</span>
+          </p>
+        </div>
+      </section>
+      <ProductTable />
+    </div>
+  );
+};
