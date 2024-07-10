b01adfd236d08db4102caa4efb44b045c6cb4ef8
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-07-09 23:04:09 -0600

    [change] Usando el fondo requerido para las páginas de login y register

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/LoginForm";
 
 export default function Login() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between">
+    <div className="flex min-h-screen flex-col items-center justify-between custom-background">
       <LoginForm/>
     </div>
   );
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -2,7 +2,7 @@ import { RegisterForm } from "@/components/RegisterForm";
 
 export default function Register() {
     return (
-      <div className="flex min-h-screen flex-col items-center justify-between">
+      <div className="flex min-h-screen flex-col items-center justify-between custom-background">
         <RegisterForm />
       </div>
     );
