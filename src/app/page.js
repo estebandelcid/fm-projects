052165d766bbf739817e6a1f09a7b8b51c456bc5
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-07-10 18:51:30 -0600

    [add] Se envolvio toda la página con el contexto

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,18 @@
+"use client"
 import { ProductTable } from "@/components/ProductTable";
 import { Sidebar } from "@/components/Sidebar";
 import { TableContainer } from "@/components/TableContainer";
+import { ProductsProvider } from "@/context/useProductsContext";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
+    <ProductsProvider>
+     <main className="flex min-h-screen">
       <Sidebar />
       <TableContainer />
     </main>
+    </ProductsProvider>
+   
   );
 }
