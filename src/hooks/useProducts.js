bb2eb071912e1524c8b2e4fed0e0fc1e521a6ef6
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-07-10 18:54:10 -0600

    [change] Solo se retorna el contexto para que se siga usando el componente en los demás componentes que lo requieren

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,43 +1,6 @@
-import { useState, useEffect } from "react";
-import useLocalStorage from "@/hooks/useLocalStorage";
-import { authKey } from "@/lib/constants";
+"use client"
+import { useProductsContext } from "@/context/useProductsContext";
 
-const API_DATA = "http://corte.fymmx.com/plantillas/getData";
-
-const useProducts = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [storedValue] = useLocalStorage(authKey);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const token = storedValue;
-      try {
-        const response = await fetch(API_DATA, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        setProducts(data);
-      } catch (error) {
-        console.error("Error fetching data: ", error);
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [storedValue]);
-
-  return { products, loading, error };
+export const useProducts = () => {
+    return(useProductsContext())
 };
-
-export default useProducts;
